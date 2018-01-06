d1fa7d3fb38f3596a865a38b6e125dd0ac3f44dc
swae <xiaoyuan1314@me.com>
2018-01-06 12:04:41 +0800

    Initial commit

---
+++ XYEmptyDataView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  XYEmptyDataView
+//
+//  Created by swae on 2018/1/6.
+//  Copyright © 2018年 alpface. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
